eabf25d50a331b48923bcab8579eb0294bddd3b4
AshleyNfonga <ashleynfonga@gmail.com>
2024-04-02 16:05:02 -0400

    I discovered my suto save feature want on
    Added constant multiplication operations
    added constant division operations

---
+++ floatBenchmark/floatBenchmark.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+using namespace std;
+using namespace std::chrono;
+
+void RunCalculations(){
+    int result;
+
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < pow(10,9); j++){
+            result = 1.0 + 1.0;
+        }
+    }
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < pow(10,9); j++){
+            result = 1.0 * 1.0;
+        }
+    }
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < pow(10,9); j++){
+            result = 1.0 / 1.0;
+        
+        }
+    }
+}
